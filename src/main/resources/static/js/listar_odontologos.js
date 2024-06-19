dcf50d4a8126878199a0513962f728677145dd8e
janet_marulanda@yahoo.com <janet_marulanda@yahoo.com>
2024-06-19 17:24:28 -0500

    se agrega sweetalert al modificar y eliminar odontologo

--- src/main/resources/static/js/listar_odontologos.js
+++ src/main/resources/static/js/listar_odontologos.js
@@ -15,15 +15,15 @@ function fetchOdontologos() {
                 const row = document.createElement("tr");
 
                 row.innerHTML = `
-                        <td>${odontologo.id}</td>
-                        <td>${odontologo.nombre}</td>
-                        <td>${odontologo.apellido}</td>
-                        <td>${odontologo.nroMatricula}</td>
-                        <td>
-                            <button class="btn btn-sm" style="background-color: #a6dcef;" data-bs-toggle="modal" data-bs-target="#editarOdontologo" onclick="editOdontologo(${odontologo.id}, '${odontologo.nombre}', '${odontologo.apellido}', '${odontologo.nroMatricula}')"><i class="fas fa-edit px-2"></i>Modificar</button>
-                            <button class="mx-4 btn btn-sm" style="background-color: #ef9a9a;" onclick="deleteOdontologo(${odontologo.id})"><i class="fas fa-trash-alt px-2"></i>Eliminar</button>
-                        </td>
-                    `;
+                    <td>${odontologo.id}</td>
+                    <td>${odontologo.nombre}</td>
+                    <td>${odontologo.apellido}</td>
+                    <td>${odontologo.nroMatricula}</td>
+                    <td>
+                        <button class="btn btn-sm" style="background-color: #a6dcef;" data-bs-toggle="modal" data-bs-target="#editarOdontologo" onclick="editOdontologo(${odontologo.id}, '${odontologo.nombre}', '${odontologo.apellido}', '${odontologo.nroMatricula}')"><i class="fas fa-edit px-2"></i>Modificar</button>
+                        <button class="mx-4 btn btn-sm" style="background-color: #ef9a9a;" onclick="deleteOdontologo(${odontologo.id})"><i class="fas fa-trash-alt px-2"></i>Eliminar</button>
+                    </td>
+                `;
 
                 tableBody.appendChild(row);
             });
@@ -34,10 +34,10 @@ function fetchOdontologos() {
 }
 
 // Modificar un odontologo
+// Modificar un odontologo
 function editOdontologo(id, nombre, apellido, nroMatricula) {
-    //Cargamos el valor del id en la variable global
+    // Cargar los valores en la ventana modal
     idOdontologo = id;
-    //Cargamos los valores a la ventana modal
     document.querySelector("#nombreEdit").value = nombre;
     document.querySelector("#apellidoEdit").value = apellido;
     document.querySelector("#matriculaEdit").value = nroMatricula;
@@ -50,7 +50,7 @@ form.addEventListener("submit", function (event) {
     const apellido = document.getElementById("apellidoEdit").value;
     const matricula = document.getElementById("matriculaEdit").value;
 
-    // llamando al endpoint de editar
+    // Llamando al endpoint de editar
     fetch(`/odontologos`, {
         method: "PUT",
         headers: {
@@ -65,30 +65,64 @@ form.addEventListener("submit", function (event) {
             var modalElement = document.getElementById('editarOdontologo');
             var modalInstance = bootstrap.Modal.getInstance(modalElement);
             modalInstance.hide();
-            alert("Odontólogo modificado con éxito");
+            Swal.fire({
+                icon: 'success',
+                title: '¡Éxito!',
+                text: 'Odontólogo modificado con éxito',
+                confirmButtonColor: '#3085d6',
+                confirmButtonText: 'Aceptar'
+            });
         })
         .catch((error) => {
             console.error("Error modificado odontólogo:", error);
+            Swal.fire({
+                icon: 'error',
+                title: '¡Error!',
+                text: 'Hubo un problema al modificar el odontólogo',
+                confirmButtonColor: '#3085d6',
+                confirmButtonText: 'Aceptar'
+            });
         });
 });
 
 // Eliminar un odontologo
 function deleteOdontologo(id) {
-    if(confirm("¿Está seguro de eliminar el odontólogo?")) {
-        // llamando al endpoint de eliminar 
-        fetch(`/odontologos/${id}`, {
-            method: "DELETE"
-        })
-            .then((response) => response.json())
-            .then((data) => {
-                console.log(data);
-                fetchOdontologos();
-                alert("Odontólogo eliminado con éxito");
+    Swal.fire({
+        title: '¿Está seguro?',
+        text: "No podrá revertir esto.",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Sí, eliminar',
+        cancelButtonText: 'Cancelar'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            fetch(`/odontologos/${id}`, {
+                method: "DELETE"
+            })
+            .then(response => {
+                if (response.ok) {
+                    fetchOdontologos();
+                    Swal.fire(
+                        'Eliminado',
+                        'El odontólogo ha sido eliminado.',
+                        'success'
+                    );
+                } else {
+                    throw new Error('Error al eliminar el odontólogo.');
+                }
             })
             .catch((error) => {
                 console.error("Error eliminando odontólogo:", error);
+                Swal.fire(
+                    'Error',
+                    'Hubo un problema al eliminar el odontólogo.',
+                    'error'
+                );
             });
-    }
+        }
+    });
 }
 
 fetchOdontologos();
